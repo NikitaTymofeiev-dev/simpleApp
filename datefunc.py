ed573421576dc70a5997e626a00f3dcd2fbe6dd4
Nikita Tymofeiev <nikita.tymofeiev@gmail.com>
2022-08-01 17:32:38 +0300

    Update datefunc.py
    add quater calculations

--- datefunc.py
+++ datefunc.py
@@ -22,15 +22,41 @@ def clear_terminal():
 
 def time_to_pay(now):
   today = now.date()
-     
+  day = now.day
+  month = now.month
+  
   end_of_month = datetime.strptime(str(today + relativedelta(day=31)) + ' 16:00', '%Y-%m-%d %H:%M')
+  
+  if end_of_quater(month):
+    return check_for_weekend(end_of_month)
+  
+  else:
+
+    if is_today_less_then_5th(day):
+      pay_will_be_soon = datetime.strptime(str(today + relativedelta(day=5)) + ' 16:00', '%Y-%m-%d %H:%M')
+      return check_for_weekend(pay_will_be_soon)
+    
+    end_of_month = end_of_month + timedelta(days=5)
+    return check_for_weekend(end_of_month)
+
+
+def end_of_quater(month):
+  if month == 3 or month == 6 or month == 9 or month == 12:
+    return True
+  return False
+    
+def is_today_less_then_5th(day):
+  if day <=5:
+    return True
+  return False
+
+def check_for_weekend(end_of_month):
   if end_of_month.weekday() == 6:
-      return end_of_month - timedelta(days=2)
+    return end_of_month - timedelta(days=2)
   if end_of_month.weekday() == 5:
-      return end_of_month - timedelta(days=1)
-
-  return datetime.strptime(str(end_of_month) + ' 16:00', '%Y-%m-%d %H:%M')
-
+    return end_of_month - timedelta(days=1)
+  return end_of_month
+  
 def time_to_vacation():
   vacation_day = input("Please enter your vacation date: (yyyy-mm-dd)\n")
 
